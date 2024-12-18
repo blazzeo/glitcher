7c423750b8d5b7aa516b9040163b47fb802678e8
blazzeo <blazzuka@gmail.com>
2024-12-18 17:12:49 +0300

    test nokhwa

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use nokhwa::{
+    self,
+    pixel_format::RgbFormat,
+    utils::{CameraIndex, RequestedFormat, RequestedFormatType},
+    Camera,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let index = CameraIndex::Index(0);
+    let req = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
+
+    let mut camera = Camera::new(index, req).unwrap();
+
+    let _ = camera.open_stream();
+    let mut frames = vec![];
+    loop {
+        let frame = camera.frame();
+        frames.push(frame);
+    }
 }
